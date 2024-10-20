763744a598f15d5d6d745acd4f5ca4fe799a92b8
Eric Ponce <ponceeric458@gmail.com>
2024-10-19 19:41:40 -0700

    navbar styling

--- Client/src/components/Navbar/Navbar.tsx
+++ Client/src/components/Navbar/Navbar.tsx
@@ -1,5 +1,6 @@
-import React from "react";
 import "./navbar.css";
+import Menu from "./icons/menu.png"
+import { navLinks } from "./constants";
 
 const Navbar = () => {
   return (
@@ -9,12 +10,17 @@ const Navbar = () => {
           style={{ backgroundColor: "green", height: "60px", width: "72px" }}
         />
         <ul className="navbar-nav-links">
-          <li>Hello World</li>
-          <li>Hello World</li>
-          <li>Hello World</li>
-          <li>Hello World</li>
+          {navLinks.map((navLink: any, idx: number) => {
+            return (
+              <li className="navbar-nav-link" key={idx}>
+                {navLink.label}
+              </li>
+            );
+          })}
+          <li className="navbar-quote-btn">Get A Quote</li>
         </ul>
       </div>
+      <img src={Menu} alt="menu" className="navbar-menu" />
     </nav>
   );
 };
---
+++ Client/src/components/Navbar/constants.ts
@@ -0,0 +1,16 @@
+const navLinks = [
+  {
+    label: "Home",
+  },
+  {
+    label: "About Us",
+  },
+  {
+    label: "Services",
+  },
+  {
+    label: "Projects",
+  },
+];
+
+export { navLinks };
