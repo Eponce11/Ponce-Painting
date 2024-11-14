bc12d41c3e9a6d993b8b8d58217a3054faac76b0
Eric Ponce <ponceeric458@gmail.com>
2024-11-13 17:17:15 -0800

    complete the grid layout for home page

--- Client/src/pages/Home/Home.tsx
+++ Client/src/pages/Home/Home.tsx
@@ -6,6 +6,14 @@ const Home = () => {
   return <div className="container">
     <Navbar />
     <div className="hero">Hero</div>
+    <div className="brands">Brands</div>
+    <div className="about-us">About Us</div>
+    <div className="services">Services</div>
+    <div className="projects">Projects</div>
+    <div className="ceo">CEO</div>
+    <div className="testimonials">Testimonials</div>
+    <div className="contact">Contact</div>
+    <div className="footer">Footer</div>
   </div>;
 };
 
