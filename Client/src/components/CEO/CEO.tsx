7efc182cbcdc5740077a83dc7f76d59b2ac8fdbb
Eric Ponce <ponceeric458@gmail.com>
2024-11-15 17:07:04 -0800

    complete ceo component

--- Client/src/components/CEO/CEO.tsx
+++ Client/src/components/CEO/CEO.tsx
@@ -1,7 +1,15 @@
 import "./ceo.scss";
+import { heading, text, btn_text, ceo_img } from "./constants";
 
 const CEO = () => {
-  return <section className="ceo">CEO</section>;
+  return (
+    <section className="ceo">
+      <h2 className="ceo__heading">{heading}</h2>
+      <p className="ceo__text">{text}</p>
+      <button className="ceo__btn">{btn_text}</button>
+      <img src={ceo_img} alt="" className="ceo__img" />
+    </section>
+  );
 };
 
 export default CEO;
---
+++ Client/src/components/CEO/constants.ts
@@ -0,0 +1,7 @@
+import Test from "../../common/imgs/project-1.png";
+
+export const heading = "Meet The Person Steering Our Success";
+export const text =
+  "Recognize the CEO’s leadership in delivering successful projects, particularly those that have made a significant impact on the community or the industry. Mention any major landmarks, infrastructure projects, or transformative buildings that were completed under their leadership. Talk about how the CEO has cultivated a positive work environment and a strong company culture";
+export const btn_text = "About CEO";
+export const ceo_img = Test;
