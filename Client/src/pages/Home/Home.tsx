e7bbcef702194609a395ba75c84bde40a39260b2
Eric Ponce <ponceeric458@gmail.com>
2024-11-27 19:10:47 -0800

    responsive about us section

--- Client/src/pages/Home/Home.tsx
+++ Client/src/pages/Home/Home.tsx
@@ -14,6 +14,7 @@ import Footer from "../../components/Footer/Footer";
 const Home = () => {
   return (
     <div className="container">
+      <About_Us />
       <Services />
       <CEO />
       <Contact />
