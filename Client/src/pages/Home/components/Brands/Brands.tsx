1b8aec615fb14d1d1c0849549ed7fc65dc214ed1
Eric Ponce <ponceeric458@gmail.com>
2024-11-28 21:54:47 -0800

    make brands page responsive

--- Client/src/pages/Home/components/Brands/Brands.tsx
+++ Client/src/pages/Home/components/Brands/Brands.tsx
@@ -2,15 +2,21 @@ import "./brands.scss";
 import Brand_1 from "../../../../common/icons/brand-logo-1.svg?react";
 
 const Brands = () => {
-  return <section className="brands">
-    <Brand_1 />
-    <Brand_1 />
-    <Brand_1 />
-    <Brand_1 />
-    <Brand_1 />
-    <Brand_1 />
-    <Brand_1 />
-  </section>;
+  return (
+    <section className="brands">
+      <div className="brands__wrapper">
+        <div className="brands__block--1">
+          <Brand_1 />
+          <Brand_1 />
+          <Brand_1 />
+        </div>
+        <div className="brands__block--2">
+          <Brand_1 />
+          <Brand_1 />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Brands;
