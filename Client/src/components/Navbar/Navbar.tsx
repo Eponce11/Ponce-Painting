35e5a3aeaf4a026456a0da7ce4d07e042b782421
Eric Ponce <ponceeric458@gmail.com>
2024-11-30 22:14:37 -0800

    add event listener

--- Client/src/components/Navbar/Navbar.tsx
+++ Client/src/components/Navbar/Navbar.tsx
@@ -1,11 +1,25 @@
 import "./navbar.scss";
 import { logo, links, btn_text } from "./constants";
+import { useState, useEffect } from "react";
 
 const Navbar = () => {
-  return (
-    <nav className="nav">
-      <div className="nav__wrapper">
-        <img src={logo} alt="Logo" className="nav__logo" />
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+
+  const handleResize = () => {
+    if (window.innerWidth > 900) {
+      setIsMenuOpen(true);
+    } else {
+      setIsMenuOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+  }, []);
+
+  const desktopNav = (
+    <>
+      {isMenuOpen && (
         <ul className="nav__list">
           {links.map((link, idx) => (
             <li className="list__item" key={idx}>
@@ -13,8 +27,30 @@ const Navbar = () => {
             </li>
           ))}
         </ul>
-        <button className="nav__btn">{btn_text}</button>
-        <div className="nav__menu-icon" />
+      )}
+      <button className="nav__btn">{btn_text}</button>
+    </>
+  );
+
+  const mobileNav = (
+    <ul className="nav__list">
+      {links.map((link, idx) => (
+        <li className="list__item" key={idx}>
+          {link}
+        </li>
+      ))}
+    </ul>
+  );
+
+  return (
+    <nav className="nav">
+      <div className="nav__wrapper">
+        <img src={logo} alt="Logo" className="nav__logo" />
+        {desktopNav}
+        <div
+          className="nav__icon"
+          onClick={() => setIsMenuOpen((prev) => !prev)}
+        />
       </div>
     </nav>
   );
