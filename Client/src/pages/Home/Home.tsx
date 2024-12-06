3181b050fa60ecb63b4d6a6a978c77b92ed355be
Eric Ponce <ponceeric458@gmail.com>
2024-12-05 17:02:55 -0800

    responsive hero section

--- Client/src/pages/Home/Home.tsx
+++ Client/src/pages/Home/Home.tsx
@@ -15,6 +15,7 @@ const Home = () => {
   return (
     <div className="container">
       <Navbar />
+      <Hero />
       <Brands />
       <About_Us />
       <Services />
--- Client/src/pages/Home/components/Hero/Hero.tsx
+++ Client/src/pages/Home/components/Hero/Hero.tsx
@@ -4,28 +4,36 @@ import Test from "../../../../common/imgs/project-1.png";
 const Hero = () => {
   return (
     <section className="hero">
-      <div className="hero__info-block">
-        <div className="hero-info-block__tag">
-          <p>Trusted Construction Partner</p>
+      <div className="hero__wrapper">
+        <div className="hero__info-block">
+          <div className="hero-info-block__tag">
+            <p>Trusted Construction Partner</p>
+          </div>
+          <h1 className="hero-info-block__heading--1">Transforming</h1>
+          <div className="hero-info-block__photo-container">
+            <h1 className="hero-info-block__heading--2">Homes and</h1>
+            <img src={Test} alt="" className="hero-info-block__photo" />
+          </div>
+          <h1 className="hero-info-block__heading--3">Commercial Spaces</h1>
+          <p className="hero-info-block__text">
+            Skyline offers a full spectrum of construction services, providing
+            tailored solutions for every project. From initial planning and
+            design to execution and completion, our team ensures precision
+          </p>
+          <div className="hero-info-block__btn-container">
+            <button className="hero-info-block__btn--1">Get Started</button>
+            <button className="hero-info-block__btn--2">
+              Explore Services
+            </button>
+          </div>
         </div>
-        <h1 className="hero-info-block__heading">
-          Comprehensive Construction Solutions By Skyline
-        </h1>
-        <img src={Test} alt="" className="hero-info-block__photo" />
-        <p className="hero-info-block__text">
-          Skyline offers a full spectrum of construction services, providing
-          tailored solutions for every project. From initial planning and design
-          to execution and completion, our team ensures precision
-        </p>
-        <button className="hero-info-block__btn--1">Get Started</button>
-        <button className="hero-info-block__btn--2">Explore Services</button>
-      </div>
-      <div className="hero__photo-block">
-        <img src={Test} alt="" className="hero-photo-block__photo--1" />
-        <img src={Test} alt="" className="hero-photo-block__photo--2" />
+        <div className="hero__photo-block"></div>
       </div>
     </section>
   );
 };
 
 export default Hero;
+/*
+<img src={Test} alt="" className="hero-photo-block__photo--1" />
+        <img src={Test} alt="" className="hero-photo-block__photo--2" />*/
