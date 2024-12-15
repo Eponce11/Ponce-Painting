4343987d720316d6b25a245018098be33d15816b
Eric Ponce <ponceeric458@gmail.com>
2024-12-15 10:51:21 -0800

    adjust css files

--- Client/src/pages/Project/Project.tsx
+++ Client/src/pages/Project/Project.tsx
@@ -1,5 +1,15 @@
+import Navbar from "../../components/Navbar/Navbar";
+import Projects from "../../components/Projects/Projects";
+import Testimonials from "../../components/Testimonials/Testimonials";
+import Footer from "../../components/Footer/Footer";
+
 const Project = () => {
-  return <div className="">Project</div>;
+  return <div className="container">
+    <Navbar />
+    <Projects />
+    <Testimonials />
+    <Footer />
+  </div>;
 };
 
 export default Project;
