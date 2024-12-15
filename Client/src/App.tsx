16dd7cd805d97ec7e54e685f865a4bea44d48965
Eric Ponce <ponceeric458@gmail.com>
2024-12-14 20:09:48 -0800

    set up routing

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,13 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./common/sass/base/_base.scss";
 import "./common/sass/base/_typography.scss";
 
 import Home from "./pages/Home/Home";
+import Project from "./pages/Project/Project";
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/project" element={<Project />} />
+      </Routes>
+    </Router>
   );
 }
 
