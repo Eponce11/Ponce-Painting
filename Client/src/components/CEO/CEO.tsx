4c9f8d1058983ba05aafb56238df98901c7a97c2
Eric Ponce <ponceeric458@gmail.com>
2025-01-01 21:48:08 -0800

    ceo routing

--- Client/src/components/CEO/CEO.tsx
+++ Client/src/components/CEO/CEO.tsx
@@ -1,12 +1,14 @@
 import "./ceo.scss";
-import { heading, text, btn_text, ceo_img } from "./constants";
+import { heading, text, btn, ceo_img } from "./constants";
 
 const CEO = () => {
   return (
     <section className="ceo">
       <h2 className="ceo__heading">{heading}</h2>
       <p className="ceo__text">{text}</p>
-      <button className="ceo__btn">{btn_text}</button>
+      <a href={`${btn.link}`} className="ceo__btn">
+        {btn.text}
+      </a>
       <img src={ceo_img} alt="" className="ceo__img" />
     </section>
   );
--- Client/src/components/CEO/constants.ts
+++ Client/src/components/CEO/constants.ts
@@ -2,5 +2,8 @@ import Test from "../../common/imgs/project-1.png";
 
 export const heading = "Meet The Person Steering Our Success";
 export const text = `Ruben Ponce, CEO of Ponce's Painting & Wallcovering, brings over 30 years of experience in the painting and wallpaper industry. His dedication to perfecting his craft has earned him a reputation for exceptional results. Under his leadership, the company has become a trusted name for commercial projects, known for quality and professionalism. Ruben ensures every project is completed with precision and care.`;
-export const btn_text = "Learn More";
+export const btn = {
+  text: "Learn More",
+  link: "/about-us",
+};
 export const ceo_img = Test;
