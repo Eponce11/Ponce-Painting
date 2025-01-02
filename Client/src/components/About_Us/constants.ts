204a149d14ea08665bd918dad52129959c2f4ca0
Eric Ponce <ponceeric458@gmail.com>
2025-01-01 22:51:05 -0800

    complete text

--- Client/src/components/About_Us/constants.ts
+++ Client/src/components/About_Us/constants.ts
@@ -3,8 +3,10 @@ import Test from "../../common/imgs/project-1.png";
 export const photo_1 = Test;
 export const photo_2 = Test;
 export const heading =
-  "Expert Team of Architects, Engineers, and Master Craftsmen";
-export const text = `Skyline offers a full spectrum of construction services, providing
-            tailored solutions for every project. From initial planning and
-            design`;
-export const btn_text = "About Us";
+  "Skilled and Trusted Team of Professionals";
+export const text = 
+"We have a team of dedicated professionals focused on providing quality service. Their expertise ensures every project is completed with precision and care.";
+export const btn = {
+  text: "About Us",
+  link: "/about-us",
+};
