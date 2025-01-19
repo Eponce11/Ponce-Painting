058a1ba26fdc60af43220a3f41810f2bab27c23d
Eric Ponce <ponceeric458@gmail.com>
2025-01-18 22:56:27 -0800

    setup privacy policy page

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -5,6 +5,7 @@ import "./common/sass/base/_typography.scss";
 import Home from "./pages/Home/Home";
 import Project from "./pages/Project/Project";
 import About from "./pages/About/About";
+import Privacy_Policy from "./pages/Privacy_Policy/Privacy_Policy";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/project/:projectName" element={<Project />} />
+        <Route path="/privacy" element={<Privacy_Policy />} />
         <Route path="/about-us" element={<About />} />
       </Routes>
     </Router>
---
+++ Client/src/pages/Privacy_Policy/Privacy_Policy.tsx
@@ -0,0 +1,11 @@
+import Navbar from "../../components/Navbar/Navbar";
+
+const Privacy_Policy = () => {
+  return (
+    <div className="container">
+      <Navbar />
+    </div>
+  );
+};
+
+export default Privacy_Policy;
