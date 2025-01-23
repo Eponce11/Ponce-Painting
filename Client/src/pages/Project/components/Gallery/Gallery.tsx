5fdc0ffdb7a3f4accb0a606ed835e0ce7849ce2e
Eric Ponce <ponceeric458@gmail.com>
2025-01-22 22:10:09 -0800

    setup modal

--- Client/src/pages/Project/components/Gallery/Gallery.tsx
+++ Client/src/pages/Project/components/Gallery/Gallery.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import "./gallery.scss";
 
 interface GalleryProps {
@@ -15,6 +16,23 @@ interface GalleryProps {
 const Gallery = (props: GalleryProps) => {
   const { photos, button_txt } = props.gallery;
 
+  const [pictureIdx, setPictureIdx] = useState<number>(0);
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+
+  const handleResize = () => {
+    if (window.innerWidth > 900) setPictureIdx(0);
+  };
+
+  useEffect(() => {
+    window.addEventListener("windowSize", handleResize);
+  }, []);
+
+  const modal = (
+    <div className="modal">
+      <div className="modal_photo" />
+    </div>
+  );
+
   return (
     <section className="gallery">
       <div className="gallery__wrapper">
@@ -26,12 +44,15 @@ const Gallery = (props: GalleryProps) => {
                 src={photo.img}
                 alt={photo.alt}
                 className={`gallery__photo gallery__photo--${idx + 1}`}
+                onClick={() => setIsModalOpen(true)}
               />
             );
           })}
         </div>
         <button className="gallery__btn">{button_txt}</button>
       </div>
+
+      {isModalOpen && modal}
     </section>
   );
 };
